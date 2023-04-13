a9fb72b67416f0b56cf029a0940006e21814c021
Joshua Micheletti <j.micheletti@campus.uniurb.it>
2023-04-13 19:43:24 +0200

    added sprite class

--- src/model/Sprite.py
+++ src/model/Sprite.py
@@ -1,3 +1,21 @@
-class Sprite():
-    def __init__():
-        print("new sprite")
+from shared import batch
+from termcolor import colored
+import pyglet
+
+class Sprite:
+    def __init__(self):
+        self.texture = None
+        self.sprite = None
+
+    def __init__(self, path):
+        self.texture = None
+        self.sprite = None
+        self.load_texture(path)
+
+    def load_texture(self, path):
+        try:
+            self.texture = pyglet.image.load(path)
+            self.sprite = pyglet.sprite.Sprite(self.texture, batch = batch)
+        except Exception as e:
+            print(colored("Failed to load image"), "red")
+            print(colored(e, "red"))
